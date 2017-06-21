2571ed830be00882c179210b8b126899b74b5bc8
Nick Landry <nick@mobility42.com>
2017-06-21 13:04:55 -0400

    Initial code commit

---
+++ Assets/Scripts/ArrowNPCMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ArrowNPCMovement : MonoBehaviour {
+
+    public GameObject targetGO;
+    private NavMeshAgent navMeshAgent;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        HeadForDestintation();
+    }
+    private void HeadForDestintation()
+    {
+        Vector3 destinaton = targetGO.transform.position;
+        navMeshAgent.SetDestination(destinaton);
+    }
+}
